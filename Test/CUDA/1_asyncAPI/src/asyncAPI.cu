26020d6d88306cdf5bcb2588f738fd1bee388ed7
Chunk <banjiaxi2011@gmail.com>
2014-09-03 19:12:56 +0800

    1_asyncAPI finished.

--- Test/CUDA/1_asyncAPI/src/asyncAPI.cu
+++ Test/CUDA/1_asyncAPI/src/asyncAPI.cu
@@ -1,135 +1,130 @@
-////////////////////////////////////////////////////////////////////////////
-//
-// Copyright 1993-2014 NVIDIA Corporation.  All rights reserved.
-//
-// Please refer to the NVIDIA end user license agreement (EULA) associated
-// with this source code for terms and conditions that govern your use of
-// this software. Any use, reproduction, disclosure, or distribution of
-// this software and related documentation outside the terms of the EULA
-// is strictly prohibited.
-//
-////////////////////////////////////////////////////////////////////////////
+/*
+ * asyncAPI.cu
+ *
+ *  Created on: Sep 3, 2014
+ *      Author: chunk
+ */
 
-//
-// This sample illustrates the usage of CUDA events for both GPU timing and
-// overlapping CPU and GPU execution.  Events are insterted into a stream
-// of CUDA calls.  Since CUDA stream calls are asynchronous, the CPU can
-// perform computations while GPU is executing (including DMA memcopies
-// between the host and device).  CPU can query CUDA events to determine
-// whether GPU has completed tasks.
-//
-
-// includes, system
 #include <stdio.h>
+#include <stdlib.h>
 
-// includes CUDA Runtime
+#include <cuda.h>
 #include <cuda_runtime.h>
-
-// includes, project
 #include <helper_cuda.h>
-#include <helper_functions.h> // helper utility functions 
+#include <helper_functions.h>
 
-__global__ void increment_kernel(int *g_data, int inc_value)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    g_data[idx] = g_data[idx] + inc_value;
+#include "util.h"
+
+__global__ void vecAdd(int* A, int* B, int* C) {
+	int i = threadIdx.x;
+	C[i] = A[i] + B[i];
 }
 
-int correct_output(int *data, const int n, const int x)
-{
-    for (int i = 0; i < n; i++)
-        if (data[i] != x)
-        {
-            printf("Error! data[%d] = %d, ref = %d\n", i, data[i], x);
-            return 0;
-        }
-
-    return 1;
+void display(int *a, int length) {
+	printf("\n");
+	for (int i = 0; i < length; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
 }
 
-int main(int argc, char *argv[])
-{
-    int devID;
-    cudaDeviceProp deviceProps;
-
-    printf("[%s] - Starting...\n", argv[0]);
-
-    // This will pick the best possible CUDA capable device
-    devID = findCudaDevice(argc, (const char **)argv);
-
-    // get device name
-    checkCudaErrors(cudaGetDeviceProperties(&deviceProps, devID));
-    printf("CUDA device [%s]\n", deviceProps.name);
-
-    int n = 16 * 1024 * 1024;
-    int nbytes = n * sizeof(int);
-    int value = 26;
-
-    // allocate host memory
-    int *a = 0;
-    checkCudaErrors(cudaMallocHost((void **)&a, nbytes));
-    memset(a, 0, nbytes);
-
-    // allocate device memory
-    int *d_a=0;
-    checkCudaErrors(cudaMalloc((void **)&d_a, nbytes));
-    checkCudaErrors(cudaMemset(d_a, 255, nbytes));
-
-    // set kernel launch configuration
-    dim3 threads = dim3(512, 1);
-    dim3 blocks  = dim3(n / threads.x, 1);
-
-    // create cuda event handles
-    cudaEvent_t start, stop;
-    checkCudaErrors(cudaEventCreate(&start));
-    checkCudaErrors(cudaEventCreate(&stop));
-
-    StopWatchInterface *timer = NULL;
-    sdkCreateTimer(&timer);
-    sdkResetTimer(&timer);
-
-    checkCudaErrors(cudaDeviceSynchronize());
-    float gpu_time = 0.0f;
-
-    // asynchronously issue work to the GPU (all to stream 0)
-    sdkStartTimer(&timer);
-    cudaEventRecord(start, 0);
-    cudaMemcpyAsync(d_a, a, nbytes, cudaMemcpyHostToDevice, 0);
-    increment_kernel<<<blocks, threads, 0, 0>>>(d_a, value);
-    cudaMemcpyAsync(a, d_a, nbytes, cudaMemcpyDeviceToHost, 0);
-    cudaEventRecord(stop, 0);
-    sdkStopTimer(&timer);
-
-    // have CPU do some work while waiting for stage 1 to finish
-    unsigned long int counter=0;
-
-    while (cudaEventQuery(stop) == cudaErrorNotReady)
-    {
-        counter++;
-    }
-
-    checkCudaErrors(cudaEventElapsedTime(&gpu_time, start, stop));
-
-    // print the cpu and gpu times
-    printf("time spent executing by the GPU: %.2f\n", gpu_time);
-    printf("time spent by CPU in CUDA calls: %.2f\n", sdkGetTimerValue(&timer));
-    printf("CPU executed %lu iterations while waiting for GPU to finish\n", counter);
-
-    // check the output for correctness
-    bool bFinalResults = (bool)correct_output(a, n, value);
-
-    // release resources
-    checkCudaErrors(cudaEventDestroy(start));
-    checkCudaErrors(cudaEventDestroy(stop));
-    checkCudaErrors(cudaFreeHost(a));
-    checkCudaErrors(cudaFree(d_a));
-
-    // cudaDeviceReset causes the driver to clean up all state. While
-    // not mandatory in normal operation, it is good practice.  It is also
-    // needed to ensure correct operation when the application is being
-    // profiled. Calling cudaDeviceReset causes all profile data to be
-    // flushed before the application exits
-    cudaDeviceReset();
-
-    exit(bFinalResults ? EXIT_SUCCESS : EXIT_FAILURE);
+bool verify(int *a, int *b, int len) {
+	for (int i = 0; i < len; i++) {
+		if (a[i] != b[i])
+			return false;
+	}
+	return true;
 }
+
+int main(int argc, char **argv) {
+	int devId;
+	cudaDeviceProp devProp;
+	devId = findCudaDevice(argc, (const char **) argv);
+	cudaGetDeviceProperties(&devProp, devId);
+
+	printf("cuda device info : %d - [%s]\n", devId, devProp.name);
+
+	const int n = 1024;
+	const int nbyte = n * sizeof(int);
+	int *h_A, *h_B, *h_C, *sum;
+	int *d_A, *d_B, *d_C;
+
+	cudaMallocHost(&h_A, nbyte);
+	cudaMallocHost(&h_B, nbyte);
+	cudaMallocHost(&h_C, nbyte);
+//	h_C = (int *) malloc(nbyte);
+	cudaMallocHost(&sum, nbyte);
+	cudaMalloc(&d_A, nbyte);
+	cudaMalloc(&d_B, nbyte);
+	cudaMalloc(&d_C, nbyte);
+	cudaMemset(&d_A, 0, nbyte);
+	cudaMemset(&d_B, 0, nbyte);
+	cudaMemset(&d_C, 0, nbyte);
+
+	cudaStream_t stream0, stream1, stream2;
+	cudaStreamCreate(&stream0);
+	cudaStreamCreate(&stream1);
+	cudaStreamCreate(&stream2);
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	StopWatchInterface *timer = NULL;
+	mTimer mtimer;
+	cuTimer cutmer;
+	sdkCreateTimer(&timer);
+
+//	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		h_A[i] = (int) (1024.0 * rand() / (RAND_MAX + 1.0));
+		h_B[i] = (int) (1024.0 * rand() / (RAND_MAX + 1.0));
+		sum[i] = h_A[i] + h_B[i];
+	}
+
+	sdkStartTimer(&timer);
+	mtimer.start();
+	cutmer.start();
+	/**
+	 *
+	 * ____
+	 * ____|___ ___ ___
+	 */
+	cudaEventRecord(start, stream0);
+	cudaMemcpyAsync(d_A, h_A, nbyte, cudaMemcpyHostToDevice, stream0);
+	cudaMemcpyAsync(d_B, h_B, nbyte, cudaMemcpyHostToDevice, stream1);
+//	cudaStreamSynchronize(stream0);
+//	cudaStreamSynchronize(stream1);
+//	vecAdd<<<1, n, 0, stream0>>>(d_A, d_B, d_C);
+//	cudaStreamSynchronize(stream0);
+	display(h_C, n);
+	checkCudaErrors(
+			cudaMemcpyAsync(h_C, d_C, nbyte, cudaMemcpyDeviceToHost, stream0));
+	cudaStreamSynchronize(stream0);
+	cudaEventRecord(stop, stream0);
+	display(h_C, n);
+	cutmer.end();
+	mtimer.end();
+	sdkStopTimer(&timer);
+
+	printf("time spent by CPU in CUDA calls: %.2f\n", sdkGetTimerValue(&timer));
+	mtimer.showTime();
+	cutmer.showTime();
+	if (verify(h_C, sum, n))
+		printf("Checking OK.\n");
+	else
+		printf("Checking Eroor!\n");
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+	cudaFreeHost(h_A);
+	cudaFreeHost(h_B);
+	cudaFreeHost(h_C);
+//	free(h_C);
+	cudaFreeHost(sum);
+	cudaFree(d_A);
+	cudaFree(d_B);
+	checkCudaErrors(cudaFree((void * )d_C));
+	checkCudaErrors(cudaDeviceReset());
+}
+
