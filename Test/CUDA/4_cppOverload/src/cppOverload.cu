b4f3e28c6744c55bd8f3e886b7347cab26a5fa30
Chunk <banjiaxi2011@gmail.com>
2014-09-05 21:49:11 +0800

    A_someSamples start.

--- Test/CUDA/4_cppOverload/src/cppOverload.cu
+++ Test/CUDA/4_cppOverload/src/cppOverload.cu
@@ -31,149 +31,147 @@ const char *sampleName = "C++ Function Overloading";
     printf("Binary Version: %d\n", attr.binaryVersion);                      \
      
 
-bool check_func1(int *hInput, int *hOutput, int a)
-{
-    for (int i = 0; i < N; ++i)
-    {
-        int cpuRes = hInput[i]*a + i;
+bool check_func1(int *hInput, int *hOutput, int a) {
+	for (int i = 0; i < N; ++i) {
+		int cpuRes = hInput[i] * a + i;
 
-        if (hOutput[i] != cpuRes)
-        {
-            return false;
-        }
-    }
+		if (hOutput[i] != cpuRes) {
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 
-bool check_func2(int2 *hInput, int *hOutput, int a)
-{
-    for (int i = 0; i < N; i++)
-    {
-        int cpuRes = (hInput[i].x + hInput[i].y)*a + i;
+bool check_func2(int2 *hInput, int *hOutput, int a) {
+	for (int i = 0; i < N; i++) {
+		int cpuRes = (hInput[i].x + hInput[i].y) * a + i;
 
-        if (hOutput[i] != cpuRes)
-        {
-            return false;
-        }
-    }
+		if (hOutput[i] != cpuRes) {
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 
-bool check_func3(int *hInput1, int *hInput2, int *hOutput, int a)
-{
-    for (int i = 0; i < N; i++)
-    {
-        if (hOutput[i] != (hInput1[i] + hInput2[i])*a + i)
-        {
-            return false;
-        }
-    }
+bool check_func3(int *hInput1, int *hInput2, int *hOutput, int a) {
+	for (int i = 0; i < N; i++) {
+		if (hOutput[i] != (hInput1[i] + hInput2[i]) * a + i) {
+			return false;
+		}
+	}
 
-    return true;
+	return true;
 }
 
-int main(int argc, const char *argv[])
-{
-    int *hInput  = NULL;
-    int *hOutput = NULL;
-    int *dInput  = NULL;
-    int *dOutput = NULL;
+int main(int argc, const char *argv[]) {
+	int *hInput = NULL;
+	int *hOutput = NULL;
+	int *dInput = NULL;
+	int *dOutput = NULL;
 
-    printf("%s starting...\n", sampleName);
+	printf("%s starting...\n", sampleName);
 
-    int deviceCount;
-    checkCudaErrors(cudaGetDeviceCount(&deviceCount));
-    printf("DevicecheckCudaErrors Count: %d\n", deviceCount);
+	int deviceCount;
+	checkCudaErrors(cudaGetDeviceCount(&deviceCount));
+	printf("DevicecheckCudaErrors Count: %d\n", deviceCount);
 
-    int deviceID = findCudaDevice(argc, argv);
+	int deviceID = findCudaDevice(argc, argv);
 	cudaDeviceProp prop;
 	checkCudaErrors(cudaGetDeviceProperties(&prop, deviceID));
-	if (prop.major < 2)    
-    {
-        printf("ERROR: cppOverload requires GPU devices with compute SM 2.0 or higher.\n");
-        printf("Current GPU device has compute SM%d.%d, Exiting...", prop.major, prop.minor);
-        exit(EXIT_SUCCESS);
-    }
-	
-    checkCudaErrors(cudaSetDevice(deviceID));
+	if (prop.major < 2) {
+		printf(
+				"ERROR: cppOverload requires GPU devices with compute SM 2.0 or higher.\n");
+		printf("Current GPU device has compute SM%d.%d, Exiting...", prop.major,
+				prop.minor);
+		exit(EXIT_SUCCESS);
+	}
 
-    // Allocate device memory
-    checkCudaErrors(cudaMalloc(&dInput , sizeof(int)*N*2));
-    checkCudaErrors(cudaMalloc(&dOutput, sizeof(int)*N));
+	checkCudaErrors(cudaSetDevice(deviceID));
 
-    // Allocate host memory
-    checkCudaErrors(cudaMallocHost(&hInput , sizeof(int)*N*2));
-    checkCudaErrors(cudaMallocHost(&hOutput, sizeof(int)*N));
+	// Allocate device memory
+	checkCudaErrors(cudaMalloc(&dInput , sizeof(int)*N*2));
+	checkCudaErrors(cudaMalloc(&dOutput, sizeof(int)*N));
 
-    for (int i = 0; i < N*2; i++)
-    {
-        hInput[i] = i;
-    }
+	// Allocate host memory
+	checkCudaErrors(cudaMallocHost(&hInput , sizeof(int)*N*2));
+	checkCudaErrors(cudaMallocHost(&hOutput, sizeof(int)*N));
 
-    // Copy data from host to device
-    checkCudaErrors(cudaMemcpy(dInput, hInput, sizeof(int)*N*2, cudaMemcpyHostToDevice));
+	for (int i = 0; i < N * 2; i++) {
+		hInput[i] = i;
+	}
 
-    // Test C++ overloading
-    bool testResult = true;
-    bool funcResult = true;
-    int a = 1;
+	// Copy data from host to device
+	checkCudaErrors(
+			cudaMemcpy(dInput, hInput, sizeof(int)*N*2, cudaMemcpyHostToDevice));
 
-    void (*func1)(const int *, int *, int);
-    void (*func2)(const int2 *, int *, int);
-    void (*func3)(const int *, const int *, int *, int);
-    struct cudaFuncAttributes attr;
+	// Test C++ overloading
+	bool testResult = true;
+	bool funcResult = true;
+	int a = 1;
 
-    // overload function 1
-    func1 = simple_kernel;
-    memset(&attr, 0, sizeof(attr));
-    checkCudaErrors(cudaFuncSetCacheConfig(*func1, cudaFuncCachePreferShared));
-    checkCudaErrors(cudaFuncGetAttributes(&attr, *func1));
-    OUTPUT_ATTR(attr);
-    (*func1)<<<DIV_UP(N, THREAD_N), THREAD_N>>>(dInput, dOutput, a);
-    checkCudaErrors(cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
-    funcResult = check_func1(hInput, hOutput, a);
-    printf("simple_kernel(const int *pIn, int *pOut, int a) %s\n\n", funcResult ? "PASSED" : "FAILED");
-    testResult &= funcResult;
+	void (*func1)(const int *, int *, int);
+	void (*func2)(const int2 *, int *, int);
+	void (*func3)(const int *, const int *, int *, int);
+	struct cudaFuncAttributes attr;
 
-    // overload function 2
-    func2 = simple_kernel;
-    memset(&attr, 0, sizeof(attr));
-    checkCudaErrors(cudaFuncSetCacheConfig(*func2, cudaFuncCachePreferShared));
-    checkCudaErrors(cudaFuncGetAttributes(&attr, *func2));
-    OUTPUT_ATTR(attr);
-    (*func2)<<<DIV_UP(N, THREAD_N), THREAD_N>>>((int2 *)dInput, dOutput, a);
-    checkCudaErrors(cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
-    funcResult = check_func2(reinterpret_cast<int2 *>(hInput), hOutput, a);
-    printf("simple_kernel(const int2 *pIn, int *pOut, int a) %s\n\n", funcResult ? "PASSED" : "FAILED");
-    testResult &= funcResult;
+	// overload function 1
+	func1 = simple_kernel;
+	memset(&attr, 0, sizeof(attr));
+	checkCudaErrors(cudaFuncSetCacheConfig(*func1, cudaFuncCachePreferShared));
+	checkCudaErrors(cudaFuncGetAttributes(&attr, *func1));
+	OUTPUT_ATTR(attr);
+	(*func1)<<<DIV_UP(N, THREAD_N), THREAD_N>>>(dInput, dOutput, a);
+	checkCudaErrors(
+			cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
+	funcResult = check_func1(hInput, hOutput, a);
+	printf("simple_kernel(const int *pIn, int *pOut, int a) %s\n\n",
+			funcResult ? "PASSED" : "FAILED");
+	testResult &= funcResult;
 
-    // overload function 3
-    func3 = simple_kernel;
-    memset(&attr, 0, sizeof(attr));
-    checkCudaErrors(cudaFuncSetCacheConfig(*func3, cudaFuncCachePreferShared));
-    checkCudaErrors(cudaFuncGetAttributes(&attr, *func3));
-    OUTPUT_ATTR(attr);
-    (*func3)<<<DIV_UP(N, THREAD_N), THREAD_N>>>(dInput, dInput+N, dOutput, a);
-    checkCudaErrors(cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
-    funcResult = check_func3(&hInput[0], &hInput[N], hOutput, a);
-    printf("simple_kernel(const int *pIn1, const int *pIn2, int *pOut, int a) %s\n\n", funcResult ? "PASSED" : "FAILED");
-    testResult &= funcResult;
+	// overload function 2
+	func2 = simple_kernel;
+	memset(&attr, 0, sizeof(attr));
+	checkCudaErrors(cudaFuncSetCacheConfig(*func2, cudaFuncCachePreferShared));
+	checkCudaErrors(cudaFuncGetAttributes(&attr, *func2));
+	OUTPUT_ATTR(attr);
+	(*func2)<<<DIV_UP(N, THREAD_N), THREAD_N>>>((int2 *) dInput, dOutput, a);
+	checkCudaErrors(
+			cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
+	funcResult = check_func2(reinterpret_cast<int2 *>(hInput), hOutput, a);
+	printf("simple_kernel(const int2 *pIn, int *pOut, int a) %s\n\n",
+			funcResult ? "PASSED" : "FAILED");
+	testResult &= funcResult;
 
-    checkCudaErrors(cudaFree(dInput));
-    checkCudaErrors(cudaFree(dOutput));
-    checkCudaErrors(cudaFreeHost(hOutput));
-    checkCudaErrors(cudaFreeHost(hInput));
+	// overload function 3
+	func3 = simple_kernel;
+	memset(&attr, 0, sizeof(attr));
+	checkCudaErrors(cudaFuncSetCacheConfig(*func3, cudaFuncCachePreferShared));
+	checkCudaErrors(cudaFuncGetAttributes(&attr, *func3));
+	OUTPUT_ATTR(attr);
+	(*func3)<<<DIV_UP(N, THREAD_N), THREAD_N>>>(dInput, dInput + N, dOutput, a);
+	checkCudaErrors(
+			cudaMemcpy(hOutput, dOutput, sizeof(int)*N, cudaMemcpyDeviceToHost));
+	funcResult = check_func3(&hInput[0], &hInput[N], hOutput, a);
+	printf(
+			"simple_kernel(const int *pIn1, const int *pIn2, int *pOut, int a) %s\n\n",
+			funcResult ? "PASSED" : "FAILED");
+	testResult &= funcResult;
 
-    checkCudaErrors(cudaDeviceSynchronize());
+	checkCudaErrors(cudaFree(dInput));
+	checkCudaErrors(cudaFree(dOutput));
+	checkCudaErrors(cudaFreeHost(hOutput));
+	checkCudaErrors(cudaFreeHost(hInput));
 
-    // cudaDeviceReset causes the driver to clean up all state. While
-    // not mandatory in normal operation, it is good practice.  It is also
-    // needed to ensure correct operation when the application is being
-    // profiled. Calling cudaDeviceReset causes all profile data to be
-    // flushed before the application exits
-    checkCudaErrors(cudaDeviceReset());
+	checkCudaErrors(cudaDeviceSynchronize());
 
-    exit(testResult ? EXIT_SUCCESS : EXIT_FAILURE);
+	// cudaDeviceReset causes the driver to clean up all state. While
+	// not mandatory in normal operation, it is good practice.  It is also
+	// needed to ensure correct operation when the application is being
+	// profiled. Calling cudaDeviceReset causes all profile data to be
+	// flushed before the application exits
+	checkCudaErrors(cudaDeviceReset());
+
+	exit(testResult ? EXIT_SUCCESS : EXIT_FAILURE);
 }
